fd40420c906515ace591aa1f4ba3a0498ce29d64
tannguyen1129 <tandtnt15@gmail.com>
2025-11-24 03:02:29 +0700

    feat(backend): Develop health advisor, fix dashborad anylistic

--- apps/notification-service/src/notification-service.controller.ts
+++ apps/notification-service/src/notification-service.controller.ts
@@ -1,9 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post, Get, HttpCode, Logger } from '@nestjs/common';
+import { NotificationServiceService } from './notification-service.service';
 
 @Controller()
 export class NotificationServiceController {
+
+  private readonly logger = new Logger(NotificationServiceController.name);
+
+  constructor(private readonly notificationService: NotificationServiceService) {}
+
   @Get()
   getHealthCheck(): string {
     return 'Notification Service is running (Active Polling Mode)';
+  }
+
+  // API MỚI: Gửi thông báo cập nhật sự cố
+  @Post('notify-incident')
+  @HttpCode(200)
+  async notifyIncidentUpdate(@Body() body: { userId: string; status: string; description: string }) {
+    this.logger.log(`📩 Yêu cầu gửi thông báo cho User ${body.userId}: ${body.status}`);
+    
+ 
+    await this.notificationService.sendIncidentNotification(body.userId, body.status, body.description);
+    
+    return { success: true };
   }
 }
